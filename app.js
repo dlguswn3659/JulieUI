1d9293191974d8ff55514162b28529ff255b0e77
hyunju <dlguswn3659@naver.com>
2020-10-01 00:02:08 +0900

    초기 파일 업로드

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+app.listen(3000, function () {
+  //3000이라는 포트를 배경으로 실행됨.
+  console.log("start! express server on port 3000!");
+}); //비동기로 동작하는 call back 함수. 스택에서 마지막에 나옴.
+
+app.use(express.static("public")); //static 디렉토리에 express를 넣는 작업
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/main", function (req, res) {
+  res.sendFile(__dirname + "/public/main.html");
+});
